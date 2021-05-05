c732b1936e3af0d0d89c9867209ebc7f2c3d96f8
trieunguyen272 <thitrieu272@gmail.com>
2021-05-06 02:06:54 +0700

    commit -m"first commit homepage"

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React dhidi
+          Learn React sdgu
         </a>
       </header>
     </div>
