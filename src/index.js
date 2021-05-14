6f3dae0f4669c5dfbcccfcb2488d07399bda0c04
trieunguyen272 <thitrieu272@gmail.com>
2021-05-14 18:52:45 +0700

    commit login

--- src/index.js
+++ src/index.js
@@ -5,24 +5,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "./style/custom-btsp.scss";
 import "mdbreact/dist/css/mdb.css";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
-import Login from "./Components/Login-Register/login";
-import Signup from "./Components/Login-Register/signup";
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <BrowserRouter>
-        
-        <Switch>
-         
-          <Route path="/login" exact component={Login} />
-          <Route path="/signup" exact component={Signup} />
-          
-        </Switch>
-
-        
-      </BrowserRouter>
+    
   </React.StrictMode>,
   document.getElementById("root")
 );
